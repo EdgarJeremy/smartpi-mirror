ea65bfb9e9bc793b20baf29bd39f1f5550e3a9c6
edgarjeremy <edgarpontoh3141@gmail.com>
2020-08-24 00:28:36 +0800

    (+) dynamic agenda and announcements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import Clock from 'react-clock';
+import SiriusAdapter from '@edgarjeremy/sirius.adapter';
 import { OpenWeatherMap } from './ReactWeather';
 import './App.css';
 import 'react-clock/dist/Clock.css';
 
+const { REACT_APP_SERVER_API, REACT_APP_SERVER_PORT } = process.env;
+
+const adapter = new SiriusAdapter(REACT_APP_SERVER_API, REACT_APP_SERVER_PORT, localStorage);
+
 class App extends React.Component {
   state = {
-    now: new Date()
+    now: new Date(),
+    models: null,
+    announcements: null,
+    agendas: null
   }
+
+  async fetchAnnouncements() {
+    const { models } = this.state;
+    const date = new Date();
+    const announcements = await models.Announcement.collection({
+      attributes: ['id', 'description', 'date'],
+      where: {
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      }
+    });
+    this.setState({ announcements });
+  }
+
+  async fetchAgendas() {
+    const { models } = this.state;
+    const date = new Date();
+    const agendas = await models.Agenda.collection({
+      attributes: ['id', 'description', 'date', 'time'],
+      where: {
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      }
+    });
+    this.setState({ agendas });
+  }
+
   componentDidMount() {
+    adapter.connect().then((models) => {
+      this.setState({ models }, () => {
+        this.fetchAnnouncements();
+        this.fetchAgendas();
+      });
+    });
     setInterval(() => {
       this.setState({ now: new Date() });
+      this.fetchAnnouncements();
+      this.fetchAgendas();
     }, 1000);
   }
   render() {
-    const { now } = this.state;
+    const { now, announcements, agendas } = this.state;
     return (
       <div>
         <div className="announcements">
-          <marquee>Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi visual seperti font, tipografi, dan tata letak.</marquee>
+          <marquee>
+            {announcements ? announcements.rows.map((a, i) => (
+              <span>[{a.date}] {a.description} — </span>
+            )) : 'memuat pengumuman...'}
+          </marquee>
         </div>
         <div className="world-info">
           <div className="clock">
@@ -34,48 +79,25 @@ class App extends React.Component {
         <div className="agendas">
           <ul className="timeline">
             <li className="timeline__line"></li>
-            <li className="timeline__item">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">08:00</time>
-                <h4 className="title">Leaving Home</h4>
-              </div>
-            </li>
-            <li className="timeline__item">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">09:30</time>
-                <h4 className="speaker">Event 01</h4>
-              </div>
-            </li>
-            <li className="timeline__item">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">11:30</time>
-                <h4 className="speaker">Event 02</h4>
-              </div>
-            </li>
-            <li className="timeline__item break">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">12:00</time>
-                <h4 className="title">Lunch</h4>
-              </div>
-            </li>
-            <li className="timeline__item">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">13:00</time>
-                <h4 className="speaker">Event 03</h4>
-              </div>
-            </li>
-            <li className="timeline__item end">
-              <div className="info">
-                <div className="dot"></div>
-                <time className="time">18:30</time>
-                <h4 className="title">Back to home</h4>
-              </div>
-            </li>
+            {agendas ? agendas.rows.map((ag, i) => (
+              i !== agendas.rows.length ? (
+                <li className="timeline__item">
+                  <div className="info">
+                    <div className="dot"></div>
+                    <time className="time">{ag.time}</time>
+                    <h4 className="title">{ag.date}</h4>
+                  </div>
+                </li>
+              ) : (
+                  <li className="timeline__item end">
+                    <div className="info">
+                      <div className="dot"></div>
+                      <time className="time">{ag.time}</time>
+                      <h4 className="title">{ag.date}</h4>
+                    </div>
+                  </li>
+                )
+            )) : (null)}
           </ul>
         </div>
       </div>
